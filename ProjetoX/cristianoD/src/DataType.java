5d2f36dfb400cece33e117ff6a887781a16f08f2
ADM <ADM@SL06MICRO06>
2017-01-13 21:15:40 -0200

    projetos pessoais

---
+++ ProjetoX/cristianoD/src/DataType.java
@@ -0,0 +1,22 @@
+
+public class DataType {
+	public static void main(String[] args) {
+		short s = 32000;
+		byte a = 127;
+		int i = 2147483647;
+		long l = 9247807;
+		float fl = (float) 100.01231315612156121615;
+		double db = 100.0;
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(fl);
+		System.out.println(db);
+		boolean quente = false;
+		System.out.println(false);
+		char c = 'a';
+		System.out.println(c);
+		char letraB = 65 + 1;
+		System.out.println(letraB);
+	}
+
+}
---
+++ ProjetoX/cristianoD/src/ZoologicoGleidson.java
@@ -0,0 +1,12 @@
+
+public class ZoologicoGleidson {
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.idade = 5;
+		a.tamanho = 7;
+		System.out.println(a.idade);
+		System.out.println(a.tamanho);
+			}
+}
+
+
